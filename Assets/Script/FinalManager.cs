e91716b6aec66668eef9efcf8587117b2970da34
Bzoth <yunusbasturk2005@gmail.com>
2024-11-03 16:37:05 +0300

    Commit 13
    FinalMusic

--- Assets/Script/FinalManager.cs
+++ Assets/Script/FinalManager.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinalManager : MonoBehaviour
 {
     public AudioSource audioSource;
-    public AudioClip Clap, UmutEnter, MertEnter, ElifEnter, YagmurEnter,ZeynepEnter, HarunEnter, Arda1, Arda2, Arda3, Arda4, Boo;
+    public AudioClip Clap, UmutEnter, MertEnter, ElifEnter, YagmurEnter,ZeynepEnter, HarunEnter, Arda1, Arda2, Arda3, Arda4, Boo, Background;
 
-    public void PlaySound(/*AudioClip clip*/)
+    public void PlaySound(AudioClip clip)
     {
-        audioSource.PlayOneShot(Clap);
+        audioSource.PlayOneShot(clip);
     }
 
     void Start()
     {
-        audioSource.PlayOneShot(Clap);
+        audioSource.PlayOneShot(Background);
+    }
+
+    public void SceneChange()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     
 }
